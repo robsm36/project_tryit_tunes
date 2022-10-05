081502b1d3c3dfb8421eec5b2a86026662538ac7
Robson Miranda <robsm36@gmail.com>
2022-10-05 16:26:00 -0300

    Criação das rotas da aplicação e cada rota deverá renderizar um componente específico.

---
+++ src/pages/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Serch extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-search">
+        <p>Login</p>
+      </div>
+    );
+  }
+}
+
+export default Serch;
