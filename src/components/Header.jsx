e020b08022dd33bffae666156f01b1a5c32ca4d2
Robson Miranda <robsm36@gmail.com>
2022-10-06 00:30:12 -0300

    Requisito 4, criação de links de navegação entre as páginas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 
 class Header extends React.Component {
@@ -21,6 +22,11 @@ class Header extends React.Component {
     return (
       <header data-testid="header-component">
         <p>Header</p>
+        <Link to="/search" data-testid="link-to-search">Search</Link>
+        <br />
+        <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+        <br />
+        <Link to="/profile" data-testid="link-to-profile">Profile</Link>
         {userName
           ? <p data-testid="header-user-name">{userName}</p>
           : <p>Carregando...</p>}
